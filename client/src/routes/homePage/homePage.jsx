66a0201484eecf223bf34c1cc005579dcdb0130d
yashasvinitripathi11 <yashasvinitripathi@gmail.com>
2024-07-25 15:22:27 +0530

    client

---
+++ client/src/routes/homePage/homePage.jsx
@@ -0,0 +1,42 @@
+import { useContext } from "react";
+import SearchBar from "../../components/searchBar/SearchBar";
+import "./homePage.scss";
+import { AuthContext } from "../../context/AuthContext";
+
+function HomePage() {
+
+  const {currentUser} = useContext(AuthContext)
+
+  return (
+    <div className="homePage">
+      <div className="textContainer">
+        <div className="wrapper">
+          <h1 className="title">Find Real Estate & Get Your Dream Place</h1>
+          <p>
+          Finding your dream home is now easier than ever with our website's comprehensive real estate services. Our platform offers a seamless and efficient way to locate the perfect place. Enjoy a hassle-free experience with detailed listings and advanced search tools. Let us guide you to your ideal home effortlessly.
+          </p>
+          <SearchBar />
+          <div className="boxes">
+            <div className="box">
+              <h1>16+</h1>
+              <h2>Years of Experience</h2>
+            </div>
+            <div className="box">
+              <h1>200</h1>
+              <h2>Award Gained</h2>
+            </div>
+            <div className="box">
+              <h1>2000+</h1>
+              <h2>Property Ready</h2>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div className="imgContainer">
+        <img src="/bg.png" alt="" />
+      </div>
+    </div>
+  );
+}
+
+export default HomePage;
