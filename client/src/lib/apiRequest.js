52c4b2be5b71d01efef722c9af06b39e025c5659
yashasvinitripathi11 <yashasvinitripathi@gmail.com>
2024-07-25 21:39:34 +0530

    p

--- client/src/lib/apiRequest.js
+++ client/src/lib/apiRequest.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const apiRequest = axios.create({
-  baseURL: "https://housing-8ogg.onrender.com//api",
+  baseURL: "https://housing-8ogg.onrender.com/api",
   withCredentials: true,
 });
 
