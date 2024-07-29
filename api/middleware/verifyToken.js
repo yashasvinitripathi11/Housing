a53f95eaa881889d2dee3a6567dce63e5cf36d53
yashasvinitripathi11 <yashasvinitripathi@gmail.com>
2024-07-29 10:53:05 +0530

    middleware

--- api/middleware/verifyToken.js
+++ api/middleware/verifyToken.js
@@ -18,7 +18,8 @@
 import jwt from "jsonwebtoken";
 
 export const verifyToken = (req, res, next) => {
-  const token = req.cookies.token || req.headers["authorization"]?.split(" ")[1];
+  const token = req.cookies.token || req.headers.authorization?.split(" ")[1];
+  console.log("Token:", token); // Check if the token is received
 
   if (!token) return res.status(401).json({ message: "Not Authenticated!" });
 
@@ -29,3 +30,4 @@ export const verifyToken = (req, res, next) => {
     next();
   });
 };
+
