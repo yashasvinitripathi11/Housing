aec1537cfa14c85000d4b549523073c2e7f90d38
yashasvinitripathi11 <yashasvinitripathi@gmail.com>
2024-07-29 10:32:43 +0530

    middleware added

--- api/middleware/verifyToken.js
+++ api/middleware/verifyToken.js
@@ -1,15 +1,31 @@
+// import jwt from "jsonwebtoken";
+
+// export const verifyToken = (req,res,next) => {
+//     const token = req.cookies.token
+//     const authHeader = req.headers["authorization"];
+
+//     if(!token) return res.status(401).json({message:"Not Authenticated!"});
+
+//     jwt.verify(token, process.env.JWT_SECRET_KEY, async(err, payload) => {
+//         if(err) return res.status(401).json({ message: "Token is not valid!"});
+
+//         req.userId = payload.id;
+
+//         next();
+//     });
+// }
+
 import jwt from "jsonwebtoken";
 
-export const verifyToken = (req,res,next) => {
-    const token = req.cookies.token
+export const verifyToken = (req, res, next) => {
+  const token = req.cookies.token || req.headers["authorization"]?.split(" ")[1];
 
-    if(!token) return res.status(401).json({message:"Not Authenticated!"});
+  if (!token) return res.status(401).json({ message: "Not Authenticated!" });
 
-    jwt.verify(token, process.env.JWT_SECRET_KEY, async(err, payload) => {
-        if(err) return res.status(401).json({ message: "Token is not valid!"});
+  jwt.verify(token, process.env.JWT_SECRET_KEY, async (err, payload) => {
+    if (err) return res.status(401).json({ message: "Token is not valid!" });
 
-        req.userId = payload.id;
-
-        next();
-    });
-}
+    req.userId = payload.id;
+    next();
+  });
+};
